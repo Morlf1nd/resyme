8d3715834ec75d704d96a292609044d7cf2ec6c9
Morlf1nd <l.kuchynskyy_fit_4_23_b_d@knute.edu.ua>
2023-12-16 04:13:27 +0200

    1

---
+++ fstream_test.cpp
@@ -0,0 +1,40 @@
+﻿#include<fstream>
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    string id;
+    string name;
+    string age;
+    string another;
+    bool yes = true;
+    ofstream test("1.txt");
+    if (!test.is_open()) {
+        cout << "Eror!!!" << endl;
+    }
+    cout << "Enter information" << endl;
+    do
+    {
+        cout << "Enter you id: ";
+        cin >> id;
+        test << id << ",";
+        cout << "Enter you name: ";
+        cin >> name;
+        test << name << ",";
+        cout << "Enter you age: ";
+        cin >> age;
+        test << age << endl;
+        cout << "Another? ";
+        cin >> another;
+        if (another == "yes") {
+            
+        }
+        else {
+            yes = false;
+        }
+    } while (yes==true);
+
+    test.close();
+}
